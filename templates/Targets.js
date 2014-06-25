08b9430dc47e735f81f457ba380ca60f74e0bc4f
overdrive3000 <linuxven@gmail.com>
2014-06-25 12:00:37 -0500

    Add Target template

---
+++ templates/Targets.js
@@ -0,0 +1,20 @@
+*** Targets ***
+
+probe = FPing
+
+menu = Top
+title = {{ target_title }}
+remark = {{ target_remark }}
+
++ Local
+
+menu = Local
+title = Local Network
+#parents = owner:/Test/James location:/
+
+++ LocalMachine
+
+menu = Local Machine
+title = This host
+host = localhost
+
